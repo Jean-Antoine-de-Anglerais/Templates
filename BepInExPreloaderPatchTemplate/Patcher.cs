68f970e0c89712c7ea21f137dd88b6781c043b97
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-22 14:54:49 +0300

    Добавьте файлы проекта.

---
+++ BepInExPreloaderPatchTemplate/Patcher.cs
@@ -0,0 +1,25 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+
+namespace BepInExPreloaderPatchTemplate
+{
+    public static class Patcher
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+        public static void Initialize()
+        {
+
+        }
+
+        public static void Finish()
+        {
+
+        }
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+
+        }
+    }
+}
