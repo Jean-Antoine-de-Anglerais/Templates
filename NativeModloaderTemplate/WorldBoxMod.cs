28faf29f9fd609c87908222e9ab4d7bfa1f90c5b
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-26 11:48:10 +0300

    Добавлен новый шаблон, "Native Modloader Template"

---
+++ NativeModloaderTemplate/WorldBoxMod.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace SimpleMod_NativeModloader
+{
+    public class WorldBoxMod : MonoBehaviour
+    {
+        static bool initialized = false;
+
+        void Update()
+        {
+            if (!Config.gameLoaded) return;
+
+
+            if (!initialized)
+            {
+                init();
+            }
+
+            initialized = true;
+        }
+
+        void init()
+        {
+
+        }
+    }
+}
